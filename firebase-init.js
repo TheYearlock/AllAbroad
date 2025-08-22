9c6235e53ccc978001af11c6d1da7932f79ca637
TheYearlock <41.yaserinan@gmail.com>
2025-08-23 02:46:24 +0300

    3.0.3

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,20 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCmTXsBNmQ8LSkEhYfEtz_h0py0jSM1L3k",
   authDomain: "allabroad-3812d.firebaseapp.com",
   projectId: "allabroad-3812d",
-  storageBucket: "allabroad-3812d.firebasestorage.app",
+  storageBucket: "allabroad-3812d.appspot.com",
   messagingSenderId: "269134466848",
   appId: "1:269134466848:web:849728e50bc9ffde70d145",
   measurementId: "G-675JECEJKJ"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
